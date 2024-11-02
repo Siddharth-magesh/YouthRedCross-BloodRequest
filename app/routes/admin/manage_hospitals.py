3010cc01d6459fcc2f254931ccef60375e0661f0
Siddharth-magesh <siddharthmagesh007@gmail.com>
2024-11-02 13:40:41 +0530

    added feature for adding new hospitals

--- app/routes/admin/manage_hospitals.py
+++ app/routes/admin/manage_hospitals.py
@@ -1,6 +1,14 @@
 from flask import Blueprint , render_template , request , flash ,redirect , url_for
 from app.models import HospitalDetails , db
 
+def get_next_id(table, prefix):
+    max_id = db.session.query(table.id).order_by(table.id.desc()).first()
+    next_id_num = 1
+    if max_id:
+        current_num = int(max_id[0][len(prefix):])
+        next_id_num = current_num + 1
+    return f"{prefix}{str(next_id_num).zfill(3)}"
+
 manage_hospital = Blueprint('manage_hospital',__name__)
 
 @manage_hospital.route('/modify_hospital_details',methods=['POST','GET'])
@@ -50,3 +58,37 @@ def modify_hospital_details():
         db.session.rollback()
         flash(f"An error occurred: {e}")
         return "An error occurred while updating the donor details", 500
+    
+@manage_hospital.route('/add_new_hospital',methods=['POST','GET'])
+def add_new_hospital():
+    hospital_id = get_next_id(HospitalDetails, 'HOSP')
+    hospital_name = request.form.get('hospital_name')
+    hospital_address = request.form.get('hospital_address')
+    city = request.form.get('city')
+    state = request.form.get('state')
+    country = request.form.get('country')
+    pincode = request.form.get('pincode')
+    branch = request.form.get('branch')
+    landmark = request.form.get('landmark')
+
+    try:
+        new_hospital = HospitalDetails(
+            id=hospital_id,
+            hospital_name=hospital_name,
+            hospital_address=hospital_address,
+            pincode=pincode,
+            city=city,
+            state=state,
+            country=country,
+            branch=branch,
+            landmark=landmark
+        )
+        db.session.add(new_hospital)
+        db.session.commit()
+        return redirect(url_for('admin.render_hospital_details_admin'))
+    except Exception as e:
+        db.session.rollback()
+        flash(f"An error occurred: {e}")
+        return "An error occurred while updating the donor details", 500
+
+    
