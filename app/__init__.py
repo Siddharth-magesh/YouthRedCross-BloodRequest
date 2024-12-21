20e3ab7aed02a3489dd86ac8a3b9686fd9c60634
vishvaa-vsk <kvishvaa6@gmail.com>
2024-12-21 15:33:09 +0530

    added a job to delete expired session files

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, current_app
 from .config import Config
 from .models import db
 from .routes import init_blueprints
@@ -7,13 +7,24 @@ from flask_session import Session
 from flask_session_captcha import FlaskSessionCaptcha
 from flask_apscheduler import APScheduler
 from app.utils.scheduled_automations import expire_blood_requests , increment_age , activate_donor_status
+import os,time
 
 scheduler = APScheduler()
+
+def clean_expired_sessions():
+    session_dir = os.path.join(os.getcwd(), 'flask_session')
+    now = time.time()
+    for filename in os.listdir(session_dir):
+        file_path = os.path.join(session_dir, filename)
+        if os.path.isfile(file_path):
+            file_age = now - os.path.getmtime(file_path)
+            if file_age > current_app.permanent_session_lifetime.total_seconds():
+                os.remove(file_path)
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    app.permanent_session_lifetime = timedelta(days=1)
+    app.permanent_session_lifetime = timedelta(minutes=2)
 
     # Initialize database
     db.init_app(app)
@@ -52,6 +63,10 @@ def configure_scheduler(app):
         with app.app_context():
             activate_donor_status()
 
+    @scheduler.task('interval', id='clean_expired_sessions_job', minutes=5)
+    def clean_expired_sessions_job():
+        with app.app_context():
+            clean_expired_sessions()
 
     scheduler.start()
 
